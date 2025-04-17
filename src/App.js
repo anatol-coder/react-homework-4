1846bbe166ee42b00d70e36372ef3e29838d9f69
anatol-coder <ankawysota@gmail.com>
2025-04-17 19:32:30 +0100

    Bootstrap installed

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import "./App.css";
+
 import Header from "./Header";
 import Main from "./Main";
 import Footer from "./Footer";
-
+import "bootstrap/dist/css/bootstrap.min.css";
 function App() {
   return (
     <div className="App">
--- src/Main.js
+++ src/Main.js
@@ -1,9 +1,21 @@
 import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./Main.css";
 
 export default function Main() {
   return (
-    <div className="Main">
-      <h2>San Francisco</h2>
+    <div className="Main" class="d-flex justify-content-between m-3 p-3">
+      <div>
+        <h2>San Francisco</h2>
+        <p>
+          Thursday 18:59, overcast clouds
+          <br />
+          Humidity: 82%, Wind: 3.13km/h
+        </p>
+      </div>
+      <div>
+        <h2>13℃</h2>
+      </div>
     </div>
   );
 }
