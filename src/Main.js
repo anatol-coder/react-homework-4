4fd7d42c1d202d5754ceb0c60ed9f453b61f6ccd
anatol-coder <ankawysota@gmail.com>
2025-04-18 09:23:47 +0100

    Main skeleton

--- src/Main.js
+++ src/Main.js
@@ -4,17 +4,26 @@ import "./Main.css";
 
 export default function Main() {
   return (
-    <div className="Main" class="d-flex justify-content-between m-3 p-3">
-      <div>
-        <h2>San Francisco</h2>
-        <p>
-          Thursday 18:59, overcast clouds
-          <br />
-          Humidity: 82%, Wind: 3.13km/h
-        </p>
+    <div className="Main">
+      <div class="d-flex justify-content-between m-2 p-3">
+        <div>
+          <h2>San Francisco</h2>
+          <p>
+            Thursday 18:59, overcast clouds
+            <br />
+            Humidity: 82%, Wind: 3.13km/h
+          </p>
+        </div>
+        <h2>13°C</h2>
       </div>
-      <div>
-        <h2>13℃</h2>
+      <div class="container m-2 p-3">
+        <div class="row">
+          <div class="col">Thu</div>
+          <div class="col">Fri</div>
+          <div class="col">Sat</div>
+          <div class="col">Sun</div>
+          <div class="col">Mon</div>
+        </div>
       </div>
     </div>
   );
